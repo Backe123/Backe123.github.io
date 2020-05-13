0ee83388e25b3ab7a7fc4d09c458d0d970db45e5
Backe123 <45962048+Backe123@users.noreply.github.com>
2020-05-13 11:06:46 +0200

    komenterat

--- IT För Alla/java.js
+++ IT För Alla/java.js
@@ -2,13 +2,13 @@
 
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/* När användaren klickar på knappen,
+växla mellan att dölja och visa innehållet */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+// Den stänger dropdown när man klickar utanför eller en gång till på knappen
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
