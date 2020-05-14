1cb5a682a6bfde5c170046109dc9e4dc2a3507cc
Backe123 <45962048+Backe123@users.noreply.github.com>
2020-05-14 20:29:17 +0200

    javaknapp

--- IT För Alla/java.js
+++ IT För Alla/java.js
@@ -1,23 +1,25 @@
 
 
+$(".dropknapp").show();
 
 
 /* När användaren klickar på knappen,
 växla mellan att dölja och visa innehållet */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+$(".dropknapp").click(function(){
+    document.getElementById("minDropdown").classList.toggle("show");
 
-// Den stänger dropdown när man klickar utanför eller en gång till på knappen
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+
+    // Den stänger dropdown när man klickar utanför eller en gång till på knappen
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropknapp')) {
+      var dropdowns = document.getElementsByClassName("dropdown-innehåll");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
       }
     }
-  }
 }
+});
