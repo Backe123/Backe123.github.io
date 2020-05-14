6a6077cd052aaa284a309cd3999599d67ed06138
Backe123 <45962048+Backe123@users.noreply.github.com>
2020-05-14 21:35:40 +0200

    n

--- IT För Alla/java.js
+++ IT För Alla/java.js
@@ -1,17 +1,21 @@
 
 
-$(".dropknapp").show();
+$(".dropknapp").show(); // Den här syns endast när javascriptet är på.
+$(".dropdown-innehåll").css("overflow, hidden");
 
 
 /* När användaren klickar på knappen,
 växla mellan att dölja och visa innehållet */
 $(".dropknapp").click(function(){
+  console.log("test");
     document.getElementById("minDropdown").classList.toggle("show");
 
-
+});
     // Den stänger dropdown när man klickar utanför eller en gång till på knappen
     window.onclick = function(event) {
+      console.log("test");
     if (!event.target.matches('.dropknapp')) {
+
       var dropdowns = document.getElementsByClassName("dropdown-innehåll");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -21,5 +25,5 @@ $(".dropknapp").click(function(){
         }
       }
     }
-  }
-});
+
+}
