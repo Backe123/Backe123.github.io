c98026d71fbc8f0cdd3dba2ebcc05cc62772995d
Backe123 <45962048+Backe123@users.noreply.github.com>
2020-05-23 18:34:17 +0200

    test

--- IT För Alla/java.js
+++ IT För Alla/java.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  // Den här syns endast när javascriptet är på.
 
   $(".dropknapp").show();
@@ -57,63 +57,5 @@ $("#list2").click(function(){  //en funktion som lyssnar efter ett tryck på #se
         scrollTop: $("#Innehåll").offset().top
     }, 1000);
 });
-=======
+
  // Den här syns endast när javascriptet är på.
-
-$(".dropknapp").show();
-  $("#minDropdown").addClass("dropdown-innehåll");
-/* När användaren klickar på knappen,
-växla mellan att dölja och visa innehållet */
-$(".dropknapp").click(function(){
-
-
-    document.getElementById("minDropdown").classList.toggle("show");
-
-
-});
-    // Den stänger dropdown när man klickar utanför knappen
-
-    window.onclick = function(event) {
-
-    if (!event.target.matches('.dropknapp')) {
-
-      var dropdowns = document.getElementsByClassName("dropdown-innehåll");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-
-}
-
-$("#change").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
-
-  $('html, body').animate({
-        scrollTop: $("#Bytaeniphone").offset().top
-    }, 1000);
-});
-
-$("#zero").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
-
-  $('html, body').animate({
-        scrollTop: $("#Nollställ").offset().top
-    }, 1000);
-});
-
-$("#list").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
-
-  $('html, body').animate({
-        scrollTop: $("#Innehåll").offset().top
-    }, 1000);
-});
-
-$("#list2").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
-
-  $('html, body').animate({
-        scrollTop: $("#Innehåll").offset().top
-    }, 1000);
-});
->>>>>>> dd23d1f74cdbc100a1eaea7e3d8e96bca426a6fe
