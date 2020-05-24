3b81447e49e51cd7a63262b93db7854d4df493e4
Backe123 <45962048+Backe123@users.noreply.github.com>
2020-05-24 15:21:38 +0200

    validerad

--- IT För Alla/java.js
+++ IT För Alla/java.js
@@ -1,11 +1,11 @@
-
+/*jslint browser: true*/ /*global  $*/
  // Den här syns endast när javascriptet är på.
 
-  $(".dropknapp").show();
-  $("#minDropdown").addClass("dropdown-innehåll");
+$(".dropknapp").show();
+$("#minDropdown").addClass("dropdown-innehall");
 /* När användaren klickar på knappen,
 växla mellan att dölja och visa innehållet */
-$(".dropknapp").click(function(){
+$(".dropknapp").click(function () {
 
 
     document.getElementById("minDropdown").classList.toggle("show");
@@ -14,47 +14,50 @@ $(".dropknapp").click(function(){
 });
     // Den stänger dropdown när man klickar utanför knappen
 
-    window.onclick = function(event) {
+// window.onclick = function(event) {
+    // if (!event.target.matches(".dropknapp")) {
+        // var dropdowns = document.getElementsByClassName("dropdown-innehall");
+        // var i;
+        // var openDropdown;
+      // for (i = 0; i < dropdowns.length; i+=1) {
+      //   openDropdown = dropdowns[i];
+      //   if (openDropdown.classList.contains("show")) {
+      //     openDropdown.classList.remove("show");
+      //   }
+      // }
+    // }
 
-    if (!event.target.matches('.dropknapp')) {
+// };
 
-      var dropdowns = document.getElementsByClassName("dropdown-innehåll");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+$("#change").click(function () {
+  //en funktion som lyssnar efter ett tryck på #section2
 
-}
-
-$("#change").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
-
-  $('html, body').animate({
+    $("html, body").animate({
         scrollTop: $("#Bytaeniphone").offset().top
     }, 1000);
 });
 
-$("#zero").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
+$("#zero").click(function () {
+  //en funktion som lyssnar efter ett tryck på #section2
 
-  $('html, body').animate({
-        scrollTop: $("#Nollställ").offset().top
+    $("html, body").animate({
+        scrollTop: $("#Nollstall").offset().top
     }, 1000);
 });
 
-$("#list").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
+$("#list").click(function () {
+  //en funktion som lyssnar efter ett tryck på #section2
 
-  $('html, body').animate({
-        scrollTop: $("#Innehåll").offset().top
+    $("html, body").animate({
+        scrollTop: $("#Innehall").offset().top
     }, 1000);
 });
 
-$("#list2").click(function(){  //en funktion som lyssnar efter ett tryck på #section2
+$("#list2").click(function () {
+  //en funktion som lyssnar efter ett tryck på #section2
 
-  $('html, body').animate({
-        scrollTop: $("#Innehåll").offset().top
+    $("html, body").animate({
+        scrollTop: $("#Innehall").offset().top
     }, 1000);
 });
 
